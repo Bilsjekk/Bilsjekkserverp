3ef61edd03d8c0fb7dd33c378b9aa60106b08025
ali99944 <ali99944@github.com>
2023-09-02 02:05:26 +0300

    upload

--- utils/mongodbConnection.js
+++ utils/mongodbConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uri = 'mongodb://admin:admin123@62.72.18.83:27017/admin'; // Replace with your MongoDB connection URI
+const uri = 'mongodb://admin:admin123@127.0.0.1:27017/admin'; // Replace with your MongoDB connection URI
 
 
 mongoose.connect(uri).then(() => console.log('connected mongoose'))
