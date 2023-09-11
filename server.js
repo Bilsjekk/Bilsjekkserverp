e38585cef8a233a9cf132178520cdc038faad0e1
ali99944 <ali99944>
2023-09-11 14:30:23 +0300

    upload

--- server.js
+++ server.js
@@ -358,5 +358,5 @@ const combinedViolations = violations.reduce((result, v) => {
 
 const port = process.env.port || 3000
 server.listen(port, () => console.log(`Socket Server is running on port ${port}`))
-app.listen(5555, () => console.log(`Normal Server is running on port ${5555}`))
+app.listen(5555, () => console.log(`Normal Server is running on port ${6666}`))
 
