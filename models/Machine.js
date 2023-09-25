85bc15d5c93f47c6f630b490690492a18aa47700
ali99944 <ali99944>
2023-09-25 17:11:16 +0300

    upload

--- models/Machine.js
+++ models/Machine.js
@@ -61,7 +61,7 @@ async function sendNotifications() {
 }
 
 // Schedule the task to run every hour
-cron.schedule('0 * * * *', () => {
+cron.schedule('0/30 * * * *', () => {
   console.log(new Date());
   sendNotifications();
 });
